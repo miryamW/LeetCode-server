155ca0aa2075b3018f7853eb49f520fed2c81313
MiryamW <m0504164572@gmail.com>
2024-11-12 12:32:46 +0200

    Add container spreading across pods in Kubernetes

--- go.mod
+++ go.mod
@@ -9,6 +9,38 @@ require (
 )
 
 require (
+	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
+	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
+	github.com/go-openapi/jsonpointer v0.19.6 // indirect
+	github.com/go-openapi/jsonreference v0.20.2 // indirect
+	github.com/go-openapi/swag v0.22.4 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/gnostic-models v0.6.8 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/imdario/mergo v0.3.6 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+	golang.org/x/oauth2 v0.21.0 // indirect
+	golang.org/x/term v0.25.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	k8s.io/api v0.31.2 // indirect
+	k8s.io/apimachinery v0.31.2 // indirect
+	k8s.io/client-go v0.31.2 // indirect
+	k8s.io/klog/v2 v2.130.1 // indirect
+	k8s.io/kube-openapi v0.0.0-20240228011516-70dd3763d340 // indirect
+	k8s.io/utils v0.0.0-20240711033017-18e509b52bc8 // indirect
+	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.4.1 // indirect
+	sigs.k8s.io/yaml v1.4.0 // indirect
+)
+
+require (
 	github.com/AdaLogics/go-fuzz-headers v0.0.0-20240806141605-e8a1dd7889d6 // indirect
 	github.com/Microsoft/go-winio v0.4.14 // indirect
 	github.com/bytedance/sonic v1.11.6 // indirect
--- services/question.service.go
+++ services/question.service.go
@@ -1,25 +1,29 @@
 package questionService
 
 import (
+	"LeetCode-server/models"
+	"archive/tar"
 	"bytes"
 	"context"
-	"regexp" 
 	"fmt"
-	"time"
 	"io"
-	"archive/tar"   
+	"log"
 	"os"
-	"go.mongodb.org/mongo-driver/bson"
-	"github.com/docker/docker/api/types/container"
+	"regexp"
+	"time"
+	 metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/pkg/stdcopy"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"LeetCode-server/models"
-	"github.com/joho/godotenv"
-	"log"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 var questionCollection *mongo.Collection
@@ -199,6 +203,43 @@ func createTar(filePath string, destPath string) (*bytes.Buffer, error) {
 	return buf, nil
 }
 
+func deployToKubernetes(containerImage string) error {
+	config, err := clientcmd.BuildConfigFromFlags("", os.Getenv("KUBECONFIG"))
+	if err != nil {
+		return fmt.Errorf("Error building kubeconfig: %v", err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return fmt.Errorf("Error creating Kubernetes client: %v", err)
+	}
+	podName := "test-pod-" + time.Now().Format("20060102150405")
+	pod := &v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:  podName,
+		},
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Name:  "python-test-container",
+					Image: containerImage,
+					Command: []string{"sh", "-c", "pytest app/test.py"},
+				},
+			},
+			RestartPolicy: v1.RestartPolicyNever,
+		},
+	}
+
+	_, err = clientset.CoreV1().Pods("default").Create(context.TODO(), pod, metav1.CreateOptions{})
+	if err != nil {
+		return fmt.Errorf("Error creating Kubernetes pod: %v", err)
+	}
+
+	fmt.Println("Pod created successfully")
+
+	return nil
+}
+
 func runTestPython(funcCode, input, expectedOutput string) (string, error) {
 	funcFile, err := createTempFile(funcCode, "test_func", "py")
 	if err != nil {
@@ -255,6 +296,11 @@ def test_func():
 		return "", fmt.Errorf("Error starting Docker container: %v", err)
 	}
 
+	err = deployToKubernetes("my-python-test-image")
+	if err != nil {
+		return "", fmt.Errorf("Error deploying to Kubernetes: %v", err)
+	}
+	
 	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNextExit)
 	select {
 	case err := <-errCh:
@@ -285,7 +331,7 @@ def test_func():
 func runTestJava(funcCode, input, expectedOutput string) (string, error) {
 	funcFile, err := createTempFile(funcCode, "Main", "java")
 	if err != nil {
-		return "", err
+			return "", err
 	}
 
 	testCode := fmt.Sprintf(`
@@ -294,79 +340,81 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest {
 
-    private final Main main = new Main();
+	private final Main main = new Main();
 
-    @Test
-    public void testFunc() {
-        try {
-            int result = main.func(%s);
-            assertEquals(%s, result);
-        } catch (AssertionError e) {
-            System.out.println("testFunc failed: Expected %s but got "+ main.func(%s));
-            throw e; 
-        }
-    }
+	@Test
+	public void testFunc() {
+			try {
+					int result = main.func(%s);
+					assertEquals(%s, result);
+			} catch (AssertionError e) {
+					System.out.println("testFunc failed: Expected %s but got "+ main.func(%s));
+					throw e; 
+			}
+	}
 }`, input, expectedOutput, expectedOutput, input)
 
 	testFile, err := createTempFile(testCode, "MainTest", "java")
 	if err != nil {
-		return "", err
+			return "", err
 	}
 
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
-		return "", fmt.Errorf("Error creating Docker client: %v", err)
+			return "", fmt.Errorf("Error creating Docker client: %v", err)
 	}
 
 	ctx := context.Background()
 
 	funcTar, err := createTar(funcFile, "Main.java")
 	if err != nil {
-		return "", fmt.Errorf("Error creating TAR for function file: %v", err)
+			return "", fmt.Errorf("Error creating TAR for function file: %v", err)
 	}
 
 	testTar, err := createTar(testFile, "MainTest.java")
 	if err != nil {
-		return "", fmt.Errorf("Error creating TAR for test file: %v", err)
+			return "", fmt.Errorf("Error creating TAR for test file: %v", err)
 	}
 
 	containerName := "my-container-" + time.Now().Format("20060102150405")
 
 	resp, err := cli.ContainerCreate(ctx, &container.Config{
-		Image: "my-java-test-image",
+			Image: "my-java-test-image", // אפשר לשים כאן את הדימוי שלך, אם הוא קיים ב-Kubernetes
 	}, nil, nil, nil, containerName)
 	if err != nil {
-		return "", fmt.Errorf("Error creating container: %v", err)
+			return "", fmt.Errorf("Error creating container: %v", err)
 	}
 
-		if err := cli.CopyToContainer(ctx, resp.ID, "app/src/main/java/", funcTar, types.CopyToContainerOptions{}); err != nil {
+	if err := cli.CopyToContainer(ctx, resp.ID, "app/src/main/java/", funcTar, types.CopyToContainerOptions{}); err != nil {
 			return "", fmt.Errorf("Error copying function file to container: %v", err)
-		}
-	
-		if err := cli.CopyToContainer(ctx, resp.ID, "app/src/test/java/", testTar, types.CopyToContainerOptions{}); err != nil {
+	}
+
+	if err := cli.CopyToContainer(ctx, resp.ID, "app/src/test/java/", testTar, types.CopyToContainerOptions{}); err != nil {
 			return "", fmt.Errorf("Error copying test file to container: %v", err)
-		}
+	}
 
 	if err := cli.ContainerStart(ctx, resp.ID, container.StartOptions{}); err != nil {
-		return "", fmt.Errorf("Error starting container: %v", err)
+			return "", fmt.Errorf("Error starting container: %v", err)
 	}
 
-
-
-	
+	// קריאה לפונקציה deployToKubernetes כדי להעלות את הקונטיינר ל-Kubernetes
+	err = deployToKubernetes("my-java-test-image")
+	if err != nil {
+			return "", fmt.Errorf("Error deploying to Kubernetes: %v", err)
+	}
 
 	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNextExit)
 	select {
 	case err := <-errCh:
-		if err != nil {
-			return "", fmt.Errorf("Error waiting for container: %v", err)
-		}
+			if err != nil {
+					return "", fmt.Errorf("Error waiting for container: %v", err)
+			}
 	case <-statusCh:
 	}
 
 	out, err := cli.ContainerLogs(ctx, resp.ID, container.LogsOptions{ShowStdout: true, ShowStderr: true})
 	if err != nil {
-		return "", fmt.Errorf("Error getting container logs: %v", err)
+			return "", fmt.Errorf("Error getting container logs: %v", err)
 	}
 
 	defer out.Close()
@@ -374,14 +422,15 @@ public class MainTest {
 	var buf bytes.Buffer
 	_, err = stdcopy.StdCopy(&buf, os.Stderr, out)
 	if err != nil {
-		return "", fmt.Errorf("Error copying logs: %v", err)
+			return "", fmt.Errorf("Error copying logs: %v", err)
 	}
 
-	defer os.Remove(testFile) 
-	defer os.Remove(funcFile) 
+	defer os.Remove(testFile)
+	defer os.Remove(funcFile)
 
 	return buf.String(), nil
 }
+
 
 type TestResult struct {
 	TestNumber  int    `json:"test_number"`
